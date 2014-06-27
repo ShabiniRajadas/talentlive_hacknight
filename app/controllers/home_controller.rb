0d05c491183b7afc605f7692d3b37b7585d69f43
Shabini <shabini@qburst.com>
2014-06-27 21:41:12 +0530

    base application with google sign in

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,71 @@
+class HomeController < ApplicationController
+	
+	def index
+	end
+	def show
+  end
+
+  def new
+    user = User.find_by_gplus(params[:gplus])
+    if user
+      redirect_to root_path(user), notice: "You are already registered!"
+    else
+      @fullname = params[:fullname]
+      @gplusId = params[:gplus]
+      u = User.new(:name => params[:fullname], :gplus => params[:gplus], :email=> params[:email])
+      if u.save
+       redirect_to root_path(u), notice: "User with name #{@fullname} and Google+ ID #{@gplusId} has been successfully registered!"
+      else
+        render "index"
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    authorize! :create, @user, :message => 'Not authorized as an administrator.'
+
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    respond_to do |format|
+
+      if @user.update_attributes(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name, :roles => [])
+    end
+end
