7ecdc008ae59fe92fc1e25c1901e32cba25481c9
manisha <manisha@qburst.com>
2014-06-28 00:22:09 +0530

    Event creation/view/edit enabled.

---
+++ app/controllers/admin/homes_controller.rb
@@ -0,0 +1,44 @@
+class Admin::HomesController < ApplicationController
+	before_filter :set_event, only: :upcoming_events
+
+	def upcoming_events
+		@upcoming_events = Event.all
+	end
+
+	def create_event
+		@event = Event.create(event_params)
+
+	    respond_to do |format|
+	      if @event.save
+	        format.html { redirect_to upcoming_events_admin_homes_path, notice: 'Event was successfully created.' }
+	      else
+	        format.html { redirect_to upcoming_events_admin_homes_path, alert: 'Event creation shows error. Please try again.' }
+	      end
+	    end
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+	    if @event.update_attributes(event_params)
+	      flash[:notice] = "Event Info Updated."
+	      redirect_to upcoming_events_admin_homes_path
+	    else
+	      flash[:alert] = @event.errors.messages
+	      redirect_to :back
+	    end
+	end
+
+	private
+
+	def set_event
+		@event = Event.new
+	end
+
+	def event_params
+      params.require(:event).permit(:name, :about, :rules, :category, :first_prize_info, :second_prize_info, :third_prize_info, :audition_start_datetime, :audition_end_datetime, :cover_pic_url)
+    end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
 	def index
 	end
 	def show
-  	end
-  	def login
-  	end
+	end
+	def login
+	end
 
   def new
     user = User.find_by_gplus(params[:gplus])
